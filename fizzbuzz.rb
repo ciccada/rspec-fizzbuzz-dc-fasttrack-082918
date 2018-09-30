bad286dba8eaacfc7a3e8f6255abe912f8c01093
Zachari Curtis <zacharijlease@gmail.com>
2018-09-30 17:19:25 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizbuzz(number)
-  if number/3 
-    puts "Fizz"
-else
-  return nil 
-  
-end 
+def fizzbuzz
 end
 
 fizbuzz(3)
